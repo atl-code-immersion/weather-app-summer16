5a224ada0d636200144b5ae28d18dcd3f149e413
Aaron Groch <ar.groch@gmail.com>
2016-08-17 19:12:35 -0400

    changed the way the results are stored in the controller - in a hash, instead of separate instance variables

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,6 +11,7 @@ class WelcomeController < ApplicationController
   	@real_feel = response["current_observation"]["feelslike_f"]
   end
 
+
   def index
   	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NE KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC PR)
 		@states.sort!
@@ -40,13 +41,15 @@ class WelcomeController < ApplicationController
 
 			response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/#{params[:state]}/#{city}.json")
 
-	  	@location = response["location"]["city"]
-	  	@temp_f = response["current_observation"]["temp_f"]
-	  	@temp_c = response["current_observation"]["temp_c"]
-	  	@weather_icon = response["current_observation"]["icon_url"]
-	  	@weather_words = response["current_observation"]["weather"]
-	  	@forecast_link = response["current_observation"]["forecast_url"] 
-	  	@real_feel = response["current_observation"]["feelslike_f"]
+	  	@results = {
+				location: response["location"]["city"],
+				temp_f: response["current_observation"]["temp_f"],
+		  	temp_c: response["current_observation"]["temp_c"],
+		  	weather_icon: response["current_observation"]["icon_url"],
+		  	weather_words: response["current_observation"]["weather"],
+		  	forecast_link: response["current_observation"]["forecast_url"], 
+		  	real_feel: response["current_observation"]["feelslike_f"],
+			}
 	  end
 
 	  @locations = Location.order(:city)
