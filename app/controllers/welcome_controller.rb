97cb35d176844ce279b6a6ba6755778d3c04cb49
Aaron Groch <ar.groch@gmail.com>
2016-08-17 19:43:13 -0400

    added a check for when no location is found

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -41,15 +41,19 @@ class WelcomeController < ApplicationController
 
 			response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/#{params[:state]}/#{city}.json")
 
-	  	@results = {
-				location: response["location"]["city"],
-				temp_f: response["current_observation"]["temp_f"],
-		  	temp_c: response["current_observation"]["temp_c"],
-		  	weather_icon: response["current_observation"]["icon_url"],
-		  	weather_words: response["current_observation"]["weather"],
-		  	forecast_link: response["current_observation"]["forecast_url"], 
-		  	real_feel: response["current_observation"]["feelslike_f"],
-			}
+			if response[:location] != nil
+		  	@results = {
+					location: response["location"]["city"],
+					temp_f: response["current_observation"]["temp_f"],
+			  	temp_c: response["current_observation"]["temp_c"],
+			  	weather_icon: response["current_observation"]["icon_url"],
+			  	weather_words: response["current_observation"]["weather"],
+			  	forecast_link: response["current_observation"]["forecast_url"], 
+			  	real_feel: response["current_observation"]["feelslike_f"],
+				}
+			else
+				@notice = response["response"]["error"]["description"]
+			end
 	  end
 
 	  @locations = Location.order(:city)
